07b89e8eff95a6ac6a1a98975a76084c1552a781
JP Sherka <jp.sherka@gmail.com>
2024-03-07 15:20:33 -0300

    add axios and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-
-import './App.css'
+import './App.css';
+import ResponsiveAppBar from '../src/header/navbar';
 
 function App() {
-
+  return (
+    <div>
+      <ResponsiveAppBar />
+    </div>
+  );
 }
 
-export default App
+export default App;
