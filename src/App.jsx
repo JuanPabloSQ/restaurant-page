f8aa4671391c6dcec20432088c0af821d4cd8afc
JP Sherka <jp.sherka@gmail.com>
2024-03-07 16:17:12 -0300

    add card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import NavBar from '../src/header/navbar';
 import SwipeableTextMobileStepper from '../src/bodypage/carousel';
+import BasicCard from './bodypage/card';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <BasicCard />
       <SwipeableTextMobileStepper />
     </div>
   );
