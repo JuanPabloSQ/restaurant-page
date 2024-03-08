d495891c95b6a49c6160f6720f7a00e51d507615
JP Sherka <jp.sherka@gmail.com>
2024-03-08 16:49:50 -0300

    fix card with bodytext

--- src/BlogPage/BlogPage.jsx
+++ src/BlogPage/BlogPage.jsx
@@ -1,6 +1,7 @@
 import ImageCard from '../utils/ImageCard';
 import NavBar from '../utils/navbar';
 import Box from '@mui/material/Box';
+import BodyText from '../BlogPage/BodyText';
 
 const Blog = () => {
   return (
@@ -17,12 +18,13 @@ const Blog = () => {
         p={2}
       >
         <ImageCard 
-        maxWidth="100%" 
-        imageWidth={1400}
-        imageHeight={500} 
-        titleText="TITULO" 
-        bodyText="MUCHO TEXTO" 
-        image="https://play-lh.googleusercontent.com/F8F_52NqhrMtlQF1Rwdxr68QO1qHzvHH2BWQjQ1LDUXChst0fOqKnRNmvaM9b6UrZIc=w526-h296-rw"
+          p={2}
+          maxWidth="100%" 
+          imageWidth={1400}
+          imageHeight={300} 
+          titleText="Descubre Nuestra Esencia Culinary: [Nombre del Restaurante] - Un Viaje a Japón en Cada Bocado"
+          bodyText={<BodyText />}
+          image="https://play-lh.googleusercontent.com/F8F_52NqhrMtlQF1Rwdxr68QO1qHzvHH2BWQjQ1LDUXChst0fOqKnRNmvaM9b6UrZIc=w526-h296-rw"
         />
       </Box>
     </div>
