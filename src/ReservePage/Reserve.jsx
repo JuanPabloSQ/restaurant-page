446cb35d2e13919b99ec5af885e1fe691e2598d8
JP Sherka <jp.sherka@gmail.com>
2024-03-09 01:35:42 -0300

    add reserve

--- src/ReservePage/Reserve.jsx
+++ src/ReservePage/Reserve.jsx
@@ -1,9 +1,49 @@
-
+import NavBar from "../utils/navbar";
+import BoxFooter from "../utils/BoxFooter";
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
 
 const Reserve = () => {
   return (
     <div>
-      <h2>Reserva</h2>
+      <NavBar />
+      <Grid 
+        container 
+        component="form"
+        spacing={1} 
+        sx={{
+          backgroundColor: '#333',  // Cambiado a gris oscuro
+          borderRadius: '8px',      // Bordes redondos
+          padding: '16px',          // Espaciado interno
+          '& input': {
+            color: 'white',
+            '&:-webkit-autofill': {
+              '-webkit-text-fill-color': 'white',
+              '-webkit-box-shadow': '0 0 0px 1000px #333 inset',  // Cambiado a gris oscuro
+            },
+          },
+          '& label': {
+            color: 'white',
+          },
+          '& fieldset': {
+            borderColor: 'white',
+          },
+        }}
+      >
+        <Grid item xs={12}>
+          <TextField fullWidth id="outlined-basic" label="Outlined" variant="outlined" />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField fullWidth id="outlined-basic" label="Outlined" variant="outlined" />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField fullWidth id="outlined-basic" label="Outlined" variant="outlined" />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField fullWidth id="outlined-basic" label="Outlined" variant="outlined" />
+        </Grid>
+      </Grid>
+      <BoxFooter />
     </div>
   );
 };
