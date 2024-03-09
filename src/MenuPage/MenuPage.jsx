1346316eedf8304063f911e07308a3fd753ced71
JP Sherka <jp.sherka@gmail.com>
2024-03-08 22:48:58 -0300

    menu map

--- src/MenuPage/MenuPage.jsx
+++ src/MenuPage/MenuPage.jsx
@@ -1,15 +1,49 @@
 import ImageCard from '../utils/ImageCard';
+import NavBar from '../utils/navbar';
+import { db } from "../firebase";
+import { collection, getDocs } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import Grid from '@mui/material/Grid';
 
 const Menu = () => {
+  const [menu, setMenu] = useState([]);
+
+  const menuCollectionRef = collection(db, "menu");
+
+  const getMenu = async () => {
+    try {
+      const dataMenu = await getDocs(menuCollectionRef);
+      console.log(dataMenu.docs);
+      setMenu(dataMenu.docs.map(doc => ({  
+        titleText: doc.data().name,
+        bodyText: `Precio: ${doc.data().value}`,  
+        image: "/path/to/your/image.jpg",  
+      })));
+    } catch (error) {
+      console.error("Error fetching menu:", error);
+    }
+  };
+
+  useEffect(() => {
+    getMenu();
+  }, []);
+
   return (
     <div>
-      <ImageCard
-        maxWidth={400}
-        imageHeight={200}
-        imagePath="/path/to/your/image.jpg"
-        title="Custom Title"
-        description="Custom description for the card."
-      />
+      <NavBar />
+      <Grid container spacing={2} justifyContent="center">
+        {menu.map((menuItem, index) => (
+          <Grid item key={index}>
+            <ImageCard
+              maxWidth={400}
+              imageHeight={200}
+              imagePath={menuItem.image}
+              titleText={menuItem.titleText}
+              bodyText={menuItem.bodyText}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
