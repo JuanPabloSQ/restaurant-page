3aac89e1747e80b8d16af50a2f2a340a98c2c3e6
JP Sherka <jp.sherka@gmail.com>
2024-03-11 00:45:34 -0300

    add contactpage funcionality

--- src/ContactPage/ContactPage.jsx
+++ src/ContactPage/ContactPage.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { db } from "../firebase";
+import { collection, addDoc } from "firebase/firestore";
 import NavBar from "../utils/NavBar";
 import BoxFooter from "../utils/BoxFooter";
 import Grid from '@mui/material/Grid';
@@ -7,6 +10,42 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 const ContactPage = () => {
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [comments, setComments] = useState('');
+
+  const handleFieldChange = (setter) => (event) => {
+    setter(event.target.value);
+  };
+
+  const handleFormSubmit = async () => {
+    const formData = {
+      name,
+      lastName,
+      email,
+      phone,
+      comments,
+    };
+
+    try {
+      const contactsCollection = collection(db, 'contacts');
+
+      const docRef = await addDoc(contactsCollection, formData);
+      console.log("Formulario de contacto enviado. Documento ID:", docRef.id);
+    
+      setName('');
+      setLastName('');
+      setEmail('');
+      setPhone('');
+      setComments('');
+    } catch (error) {
+      console.error("Error al enviar el formulario de contacto:", error);
+      
+    }
+  };
+
   return (
     <div>
       <NavBar />
@@ -14,12 +53,12 @@ const ContactPage = () => {
         display="flex"
         justifyContent="center"
         alignItems="center"
-        sx={{ paddingLeft: '200px', paddingRight: '200px', marginTop: '150px' }} 
+        sx={{ paddingLeft: '200px', paddingRight: '200px', marginTop: '150px' }}
       >
-        <Grid 
-          container 
+        <Grid
+          container
           component="form"
-          spacing={1} 
+          spacing={1}
           sx={{
             backgroundColor: '#333',
             borderRadius: '8px',
@@ -45,16 +84,44 @@ const ContactPage = () => {
             </Typography>
           </Grid>
           <Grid item xs={6}>
-            <TextField fullWidth id="name" label="Tu nombre" variant="outlined" />
+            <TextField
+              fullWidth
+              id="name"
+              label="Tu nombre"
+              variant="outlined"
+              value={name}
+              onChange={handleFieldChange(setName)}
+            />
           </Grid>
           <Grid item xs={6}>
-            <TextField fullWidth id="lastName" label="Tu apellido" variant="outlined" />
+            <TextField
+              fullWidth
+              id="lastName"
+              label="Tu apellido"
+              variant="outlined"
+              value={lastName}
+              onChange={handleFieldChange(setLastName)}
+            />
           </Grid>
           <Grid item xs={6}>
-            <TextField fullWidth id="mail" label="Email" variant="outlined" />
+            <TextField
+              fullWidth
+              id="email"
+              label="Email"
+              variant="outlined"
+              value={email}
+              onChange={handleFieldChange(setEmail)}
+            />
           </Grid>
           <Grid item xs={6}>
-            <TextField fullWidth id="phone" label="Teléfono" variant="outlined" />
+            <TextField
+              fullWidth
+              id="phone"
+              label="Teléfono"
+              variant="outlined"
+              value={phone}
+              onChange={handleFieldChange(setPhone)}
+            />
           </Grid>
           <Grid item xs={12}>
             <TextField
@@ -64,10 +131,14 @@ const ContactPage = () => {
               multiline
               maxRows={4}
               variant="outlined"
+              value={comments}
+              onChange={handleFieldChange(setComments)}
             />
           </Grid>
           <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', marginTop: '16px' }}>
-            <Button variant="contained">Enviar</Button>
+            <Button variant="contained" onClick={handleFormSubmit}>
+              Enviar
+            </Button>
           </Grid>
         </Grid>
       </Box>
