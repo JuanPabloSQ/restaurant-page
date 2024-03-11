0ab863e1e5e72248fd844cc79799a4d57cb306e0
JP Sherka <jp.sherka@gmail.com>
2024-03-11 15:10:43 -0300

    improvement menu

--- src/MenuPage/MenuPage.jsx
+++ src/MenuPage/MenuPage.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react';
 import Grid from '@mui/material/Grid';
 import BoxFooter from '../utils/BoxFooter';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const Menu = () => {
   const [menu, setMenu] = useState([]);
@@ -19,7 +20,8 @@ const Menu = () => {
       setMenu(dataMenu.docs.map(doc => ({  
         titleText: doc.data().name,
         bodyText: `Precio: ${doc.data().value}`,  
-        image: doc.data().image,  
+        image: doc.data().image,
+        category: doc.data().category,
       })));
     } catch (error) {
       console.error("Error fetching menu:", error);
@@ -30,31 +32,52 @@ const Menu = () => {
     getMenu();
   }, []);
 
+  const renderCategorySection = (categoryTitle, categoryMenu) => {
+    return (
+      <div key={categoryTitle}>
+        <Typography variant="h4" color="primary" style={{ marginTop: '100px', marginBottom: '10px' }}>
+          {categoryTitle}
+        </Typography>
+        <Grid 
+          display="flex" 
+          justifyContent="center" 
+          alignItems="center"
+          container 
+          spacing={2} 
+          style={{ marginLeft: '10px', marginBottom: "50px" }} 
+        >
+          {categoryMenu.map((menuItem, index) => (
+            <Grid item key={index} xs={12} sm={6} md={4}>
+              <ImageCard
+                maxWidth={400}
+                imageHeight={200}
+                image={menuItem.image}
+                titleText={menuItem.titleText}
+                bodyText={menuItem.bodyText}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    );
+  };
+
+  const ramenMenu = menu.filter(item => item.category === 'ramen');
+  const sushiMenu = menu.filter(item => item.category === 'sushi');
+  const bebidaMenu = menu.filter(item => item.category === 'bebida');
+  const otrosPlatosMenu = menu.filter(item => item.category === 'otros platos');
+
   return (
     <Box>
       <Grid>
         <NavBar />
       </Grid>
-      <Grid 
-        display="flex" 
-        justifyContent="center" 
-        alignItems="center"
-        container 
-        spacing={2} 
-        style={{ marginLeft: '10px', marginTop: "80px", marginBottom: "50px" }} 
-      >
-        {menu.map((menuItem, index) => (
-          <Grid item key={index} xs={12} sm={6} md={4}>
-            <ImageCard
-              maxWidth={400}
-              imageHeight={200}
-              image={menuItem.image}
-              titleText={menuItem.titleText}
-              bodyText={menuItem.bodyText}
-            />
-          </Grid>
-        ))}
-      </Grid>
+
+      {ramenMenu.length > 0 && renderCategorySection('Ramen:', ramenMenu)}
+      {sushiMenu.length > 0 && renderCategorySection('Sushi:', sushiMenu)}
+      {bebidaMenu.length > 0 && renderCategorySection('Bebidas:', bebidaMenu)}
+      {otrosPlatosMenu.length > 0 && renderCategorySection('Otros Platos:', otrosPlatosMenu)}
+
       <BoxFooter />
     </Box>
   );
