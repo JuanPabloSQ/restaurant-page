027f4d0efc90836e895fc76ecdda90b3a6137555
JP Sherka <jp.sherka@gmail.com>
2024-03-11 11:55:17 -0300

    add funcionality contactpage

--- src/ContactPage/ContactPage.jsx
+++ src/ContactPage/ContactPage.jsx
@@ -8,6 +8,16 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { useSnackbar } from "../utils/SnackBarContext";
+import * as yup from 'yup';
+
+const validationSchema = {
+  name: yup.string().required('El nombre es obligatorio'),
+  lastName: yup.string().required('El apellido es obligatorio'),
+  email: yup.string().email('Ingrese un correo electrónico válido').required('El correo electrónico es obligatorio'),
+  phone: yup.number().typeError('Ingrese un número').required('El teléfono es obligatorio'),
+  comments: yup.string().required('Ingrese sus comentarios').max(1000, 'Los comentarios deben tener como máximo 1000 caracteres'),
+};
 
 const ContactPage = () => {
   const [name, setName] = useState('');
@@ -16,34 +26,83 @@ const ContactPage = () => {
   const [phone, setPhone] = useState('');
   const [comments, setComments] = useState('');
 
-  const handleFieldChange = (setter) => (event) => {
-    setter(event.target.value);
+  const [formErrors, setFormErrors] = useState({
+    name: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    comments: '',
+  });
+
+  const validateField = (fieldName, value) => {
+    try {
+      yup.reach(yup.object(validationSchema), fieldName).validateSync(value, { abortEarly: false });
+      console.log(`Validación exitosa para ${fieldName}`);
+      setFormErrors((prevErrors) => ({ ...prevErrors, [fieldName]: '' }));
+    } catch (error) {
+      console.error(`Validación fallida para ${fieldName}:`, error);
+      setFormErrors((prevErrors) => ({
+        ...prevErrors,
+        [fieldName]: error.errors[0],
+      }));
+    }
   };
 
+  const handleFieldChange = (setter) => (event) => {
+    const fieldName = event.target.id;
+    const value = event.target.value;
+    validateField(fieldName, value);
+    setter(value);
+  };
+
+  const { errorSnackbar } = useSnackbar();
+  const { successSnackbar } = useSnackbar();
+
   const handleFormSubmit = async () => {
-    const formData = {
-      name,
-      lastName,
-      email,
-      phone,
-      comments,
-    };
-
     try {
-      const contactsCollection = collection(db, 'contacts');
+      Object.keys(validationSchema).forEach(fieldName => {
+        validateField(fieldName, eval(fieldName));
+      });
 
+      const formData = {
+        name,
+        lastName,
+        email,
+        phone,
+        comments,
+      };
+
+      const contactsCollection = collection(db, 'contacts');
       const docRef = await addDoc(contactsCollection, formData);
+      successSnackbar('Mensaje enviado con éxito ');
       console.log("Formulario de contacto enviado. Documento ID:", docRef.id);
-    
+
       setName('');
       setLastName('');
       setEmail('');
       setPhone('');
       setComments('');
     } catch (error) {
-      console.error("Error al enviar el formulario de contacto:", error);
-      
+      if (error.name === 'ValidationError') {
+        errorSnackbar('Error con el servidor');
+        console.error("Error de validación:", error.errors);
+        const errors = {};
+        error.inner.forEach((err) => {
+          errors[err.path] = err.message;
+        });
+        applyFormErrors(errors);
+      } else {
+        console.error("Error al enviar el formulario de contacto:", error);
+      }
     }
+  };
+
+  const applyFormErrors = (error) => {
+    setFormErrors(
+      error.inner.reduce((acc, curr) => {
+        return { ...acc, [curr.path]: curr.errors[0] };
+      }, {}),
+    );
   };
 
   return (
@@ -91,6 +150,8 @@ const ContactPage = () => {
               variant="outlined"
               value={name}
               onChange={handleFieldChange(setName)}
+              error={Boolean(formErrors.name)}
+              helperText={formErrors.name}
             />
           </Grid>
           <Grid item xs={6}>
@@ -101,6 +162,8 @@ const ContactPage = () => {
               variant="outlined"
               value={lastName}
               onChange={handleFieldChange(setLastName)}
+              error={Boolean(formErrors.lastName)}
+              helperText={formErrors.lastName}
             />
           </Grid>
           <Grid item xs={6}>
@@ -111,6 +174,8 @@ const ContactPage = () => {
               variant="outlined"
               value={email}
               onChange={handleFieldChange(setEmail)}
+              error={Boolean(formErrors.email)}
+              helperText={formErrors.email}
             />
           </Grid>
           <Grid item xs={6}>
@@ -121,6 +186,8 @@ const ContactPage = () => {
               variant="outlined"
               value={phone}
               onChange={handleFieldChange(setPhone)}
+              error={Boolean(formErrors.phone)}
+              helperText={formErrors.phone}
             />
           </Grid>
           <Grid item xs={12}>
@@ -133,6 +200,8 @@ const ContactPage = () => {
               variant="outlined"
               value={comments}
               onChange={handleFieldChange(setComments)}
+              error={Boolean(formErrors.comments)}
+              helperText={formErrors.comments}
             />
           </Grid>
           <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', marginTop: '16px' }}>
