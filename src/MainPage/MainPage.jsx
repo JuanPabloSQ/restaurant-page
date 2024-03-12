9093133104c996bef3f504fe44b629641b23609b
JP Sherka <jp.sherka@gmail.com>
2024-03-11 23:07:49 -0300

    add image main page

--- src/MainPage/MainPage.jsx
+++ src/MainPage/MainPage.jsx
@@ -4,11 +4,14 @@ import Box from '@mui/material/Box';
 import BoxFooter from '../utils/BoxFooter';
 
 const MainPage = () => {
+  const imageUrl = 'https://c4.wallpaperflare.com/wallpaper/70/192/37/greens-fish-figure-black-background-wallpaper-preview.jpg';
+
   return (
-    <Box >
+    <Box>
       <NavBar />
-      <Box display="flex" justifyContent="space-between" marginTop={10} p={2} >
-        <BasicCard  />
+      <Box display="flex" justifyContent="space-between" alignItems="center" marginTop={10} p={2}>
+        <BasicCard />
+        <img src={imageUrl} alt="Fish" style={{ width: '50%', marginRight: '20px' }} />
       </Box>
       <BoxFooter />
     </Box>
